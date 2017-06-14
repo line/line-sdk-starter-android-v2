e14458c65002ca24f80a13a62b508690232ee018
Serrano Mark <JP11686@Siera20170308-784f43854f34.local>
2017-06-14 19:19:21 +0900

    Removed button import because it is unnecessary

--- app/src/main/java/jp/line/android/sdk/sample/MainActivity.java
+++ app/src/main/java/jp/line/android/sdk/sample/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.linecorp.linesdk.auth.LineLoginApi;
